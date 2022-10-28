ac99676a8d884ad64cbf36f9fd32b17a7451c656
Egor Solodovnikov <egor.solo1@yandex.ru>
2022-10-28 19:52:18 +0200

    populate pages with no types defined

---
+++ pages/news.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import getNewsArticles from '../utils/getNewsArticles'
+
+const DEFAULT_IMAGE =
+  'https://fashionunited.info/global-assets/img/default/fu-default_1200x630_black-favicon.jpg'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { newsArticles: [] }
+  }
+
+  async componentWillMount() {
+    const variables = {
+      keywords: ['hunkemoller'],
+    }
+    const result = await getNewsArticles(variables)
+    this.setState({
+      newsArticles: result.fashionunitedNlNewsArticles,
+    })
+  }
+
+  newsArticles() {
+    return this.state.newsArticles.map((newsArticle, index) => (
+      <div key={index}>
+        <h2>{newsArticle.title}</h2>
+        <img src={newsArticle.imageUrl || DEFAULT_IMAGE} />
+        <div>
+          {newsArticle.description} <a href={newsArticle.url}>Read more</a>
+        </div>
+      </div>
+    ))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Fashion News</h1>
+        <div>{this.newsArticles()}</div>
+      </div>
+    )
+  }
+}
+export default App
---
+++ pages/news2.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const news2 = () => {
+  return <div></div>
+}
+
+export default news2
